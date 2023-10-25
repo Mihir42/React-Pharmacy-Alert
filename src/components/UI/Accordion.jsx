153bbbe1f7b13a9a556196e3d2f01c09ca5eca16
Mihir42 <mihir_mistry@outlook.com>
2023-10-26 00:19:12 +0100

    Able to fetch from backend, and accordion interfac

---
+++ src/components/UI/Accordion.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import "./Accordion.scss";
+
+function Accordion(props) {
+  const title = props.title;
+  const description = props.description;
+
+  const [isActive, setIsActive] = useState(false);
+
+  //State ---------------------------------------
+
+  return (
+    <>
+      <div className="accordion">
+        <div className="accordion-item">
+          <div
+            className="accordion-title"
+            onClick={() => setIsActive(!isActive)}
+          >
+            <div>{title}</div>
+            <div>{isActive ? "-" : "+"}</div>
+          </div>
+          {isActive && (
+            <div className="accordion-description">{description}</div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+Accordion.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+
+export default Accordion;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect } from "react";
+import Accordion from "../UI/Accordion";
+import API from "../api/API";
+
 function Home() {
-  // Properties -----------------------------
-  // Hooks ----------------------------------
+  // Initialisation -----------------------------
+  const loggedInUserID = 1001;
+  const endpoint = `/drugs/${loggedInUserID}`;
+
+  // State ----------------------------------
+  const [drugs, setDrugs] = useState(null);
+  const [loadingMessage, setLoadingMessage] = useState("Loading Records ...");
+
   // Context --------------------------------
   // Methods --------------------------------
+  const apiCall = async (endpoint) => {
+    const response = await API.get(endpoint, "GET");
+    response.isSuccess
+      ? setDrugs(response.result)
+      : setLoadingMessage(response.message);
+  };
+
+  useEffect(() => {
+    apiCall(endpoint);
+  }, [endpoint]);
+
   // View -----------------------------------
-  return <h1>Home</h1>;
+  return (
+    <section>
+      {!drugs ? (
+        <p>{loadingMessage}</p>
+      ) : drugs.length === 0 ? (
+        <p>No drugs Found</p>
+      ) : (
+        drugs.map((drug) => (
+          <Accordion
+            key={drug.DrugID}
+            title={drug.DrugName}
+            description={drug.DrugDosage}
+          ></Accordion>
+        ))
+      )}
+    </section>
+  );
 }
 
 export default Home;
